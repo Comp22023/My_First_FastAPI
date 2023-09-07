2714fe65dded794afe4dfe6cc8322be99174df91
Comp022023 <kyurinova@inbox.ru>
2023-09-07 10:51:00 +0300

    Добавление пути

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {'Найдите секретный путь, который выдаст номер вашего билета'}
+
 @app.get("/sum/{var1}/{var2}")
 async def sum(var1: int, var2: int):
     return {"Ваш номер билета": var1+var2}
