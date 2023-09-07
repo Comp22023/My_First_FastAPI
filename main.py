4b4295e8c400f06d989a006beae5784b51a4c3cc
Comp022023 <kyurinova@inbox.ru>
2023-09-07 10:29:51 +0300

    Изменение пути, сложение двух переменных

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Привет"}
+@app.get("/sum/{var1}/{var2}")
+async def sum(var1: int, var2: int):
+    return {"Ваш номер билета": var1+var2}
